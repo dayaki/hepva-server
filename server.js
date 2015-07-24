19067b3ae006ceef67abdd1cd78e6dedecc92868
Dayo Akinkuowo <me@sprypixels.com>
2015-07-24 19:31:57 +0100

    Update server.js to testing envr

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
 var mongo = require('mongodb').MongoClient,
-    client = require('socket.io').listen(8080).sockets;
+    restify = require('restify'),
+    mongojs = require('mongojs'),
+    port = process.env.PORT || 3000,
+    io = require('socket.io').listen(port),
+    server = restify.createServer();
+ 
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
 
-mongo.connect('mongodb://104.131.222.95/hepva', function(err, db) {
-  if (err) throw err;
+var db = mongojs('mongodb://127.0.0.1:27017/hepva', ['users']);
 
-  client.on('connection', function(socket) {
-  	console.log(socket.id);
-  	var col = db.collection('messages');
-
-  	// Emit all messages
-  	col.find().limit(50).sort({_id: 1}).toArray(function(err, res) {
-  		if (err) throw err;
-  		socket.emit('all', res);
-  	});
-
-    // wait for input
-    socket.on('new message', function(data) {
-    	var name = data.name,
-    			message = data.message;
-
-    	col.insert(data, function() {
-    		console.log('Inserted');
-    	})
-    })
-  });
-
+server.listen(port, function () {
+  console.log("Server started @  port:" +port);
 });
 
+server.get("/", function (req, res, next) {
+  res.send('Thank you for showing up here');
+    return next();
+});
+
+server.get("/users", function (req, res, next) {
+  db.users.find(function (err, users) {
+      res.writeHead(200, {
+          'Content-Type': 'application/json; charset=utf-8'
+      });
+      res.end(JSON.stringify(users));
+  });
+  return next();
+});
+
+server.post('/users', function (req, res, next) {
+  var user = req.params;
+  db.users.save(user, function (err, data) {
+    res.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8'
+    });
+    // res.end(JSON.stringify(data));
+    res.status(202).end();
+  });
+  return next();
+});
